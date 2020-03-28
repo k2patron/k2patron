e3c4e9ce7bd193d6ae0b6ea57bde9367bbe74acd
k2patron <k2patron@gmail.com>
2020-03-28 22:34:06 +0300

    Проба другой сборки

---
+++ engine/ajax/updates.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DATALIFEENGINE', true);
+include ENGINE_DIR.'/data/config.php';
+@header("Content-type: text/html; charset=".$config['charset']);
+?>
+Проверка обновлений отключена в целях безопасности. Посетите <a href="http://anonym.to/http://dle-news.ru/" target="_blank">dle-news.ru</a>, чтобы узнать актуальную версию.
